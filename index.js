3abbd1b71246b4da209fbc9adb64e0f5588ab978
ctrl-root <muriithidennis340@gamil.com>
2023-05-17 12:53:39 +0300

    code commit

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
+
 dbConnect();
 
 app.use("/", (req, res) => {
     res.send("Hello from server side");
 });
+
+app.use("/api/user, authRouter");
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
---
+++ routes/authRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const rouute = express.Router();
+
+router.post("/register");
+module.exports = router;
