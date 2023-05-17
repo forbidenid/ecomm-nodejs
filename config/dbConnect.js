30c526aeccefb75e59856f400818dba69500decf
ctrl-root <muriithidennis340@gamil.com>
2023-05-17 12:11:14 +0300

    code commit

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,12 @@
 const { default: mongoose, sanitizeFilter } = require("mongoose");
 
 const dbConnect = () => {
-    const conn = mongoose.connect("mongodb")
+    try {
+        const conn = mongoose.connect("mongodb://localhost:27017/mydatabase");
+        console.log("Database connected succesefully");
+    } catch (error) {
+        console.log("Database error");
+    }
+
 };
+module.exports= dbConnect;
