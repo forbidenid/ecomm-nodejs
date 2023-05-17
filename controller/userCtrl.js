ee91499ada6e9a099566086d75cdeade21f581c0
ctrl-root <muriithidennis340@gamil.com>
2023-05-17 18:47:37 +0300

    commit changes

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,4 +1,5 @@
 
+const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
@@ -17,7 +18,14 @@ const loginUserCtrl=asyncHandler(async(req, res) => {
     const { email, password } = req .body;
     const findUser = await User.findOne({ email });
     if ( findUser && await findUser.isPasswordMatched(password)) {
-        res.json(findUser);
+        res.json({
+            id: findUser?. id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
     } else {
         throw new Error ("Invalid Credentials");
     }
