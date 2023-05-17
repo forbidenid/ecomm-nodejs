448091b2475f2c801df01384d44609327b3b9e2b
ctrl-root <muriithidennis340@gamil.com>
2023-05-17 15:16:28 +0300

    code commit

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -3,16 +3,16 @@ const User = require("../models/userModel");
 
 const createUser = async (req,res) => {
     const email = req.body.email
-    const findUser = await User.findOne(email);
+    const findUser = await User.find({ email: email });
     if(!findUser){
         const newUser = User.create(req.body);
         res.json(newUser);
     } else {
         res.json({
             msg: "User Already User Exists",
-            success:false
+            success:false,
         });
 
     }
 };
-module.exports={createUser};
+module.exports={ createUser };
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
 
 dbConnect();
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/user', authRouter);
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
