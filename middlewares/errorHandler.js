f95188231d4f6f8c415ab9d86169508f03b7d949
ctrl-root <muriithidennis340@gamil.com>
2023-05-18 11:02:28 +0300

    changes commit

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,15 +1,22 @@
+// not Found
+
 const notFound = (req, res, next) => {
     const error = new Error(`Not Found : ${req.originalUrl}`);
     res.status(404);
     next(error);
-
-};
-const errorHandler = (err, req, res, next) => {
-    const statuscode = req.statusCode == 200 ? 500 : res.statusCode;
+  };
+  
+  // Error Handler
+  
+  const errorHandler = (err, req, res, next) => {
+    const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
     res.status(statuscode);
-    req.json({
-        message: err?.message,
-        stack: err?.stack, 
+    res.json({
+      status: "fail",
+      message: err?.message,
+      stack: err?.stack,
     });
-};
-module.exports = { errorHandler, notFound };
+  };
+  
+  module.exports = { errorHandler, notFound };
+  
